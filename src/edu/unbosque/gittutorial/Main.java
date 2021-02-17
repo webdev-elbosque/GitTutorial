cd5888567fde495db15b20115395b14ad77308a4
Fabian Peña <fabiancpl91@gmail.com>
2021-02-17 14:53:19 -0500

    Extending message

--- src/edu/unbosque/gittutorial/Main.java
+++ src/edu/unbosque/gittutorial/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo! Estamos en el curso de Git y GitHub.");
     }
 
 }
