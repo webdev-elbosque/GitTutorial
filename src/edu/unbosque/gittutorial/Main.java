b39cd716e382ef302b74ab8ad53acfab8fa0c53f
Fabian Peña <fabiancpl91@gmail.com>
2021-02-17 15:00:52 -0500

    Adding current date in message

--- src/edu/unbosque/gittutorial/Main.java
+++ src/edu/unbosque/gittutorial/Main.java
@@ -1,10 +1,15 @@
 package edu.unbosque.gittutorial;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args)
     {
+        Date currentDate = new Date();
+
         System.out.println("Hola mundo! Estamos en el curso de Git y GitHub.");
+        System.out.println("The current date is " + currentDate.toString());
     }
 
 }
